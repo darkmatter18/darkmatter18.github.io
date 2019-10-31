df0e6cad4ee402ee5d06a8764c899d3da00863e8
Arkadip <in2arkadipb13@gmail.com>
2019-10-31 13:41:08 +0530

    Initial Build

---
+++ precache-manifest.b4b4ed6b87de00f4ba1f81b42599e818.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da95a5c8295f73d19231cf35b6ba610f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b852d2e70f270146f9a",
+    "url": "/static/css/2.a641e29a.chunk.css"
+  },
+  {
+    "revision": "443df55432aa07ed8e97",
+    "url": "/static/css/main.79daaf87.chunk.css"
+  },
+  {
+    "revision": "2b852d2e70f270146f9a",
+    "url": "/static/js/2.856be6d7.chunk.js"
+  },
+  {
+    "revision": "443df55432aa07ed8e97",
+    "url": "/static/js/main.0a10c13b.chunk.js"
+  },
+  {
+    "revision": "0dbcb36fd6814a979401",
+    "url": "/static/js/runtime~main.81fcde1f.js"
+  },
+  {
+    "revision": "47ce11be1a57ccd6be2e9b8bcb24df8c",
+    "url": "/static/media/logo.47ce11be.jpeg"
+  }
+]);
